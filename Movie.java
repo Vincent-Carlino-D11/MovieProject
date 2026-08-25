07f64cd9c37b6776d9e0fdd760abdeeb3c38f32a
Oliver Kirkevold-Corder <oliver.kirkevold-corder@d11.org>
2026-08-25 11:54:45 -0600

    added color to print

--- Movie.java
+++ Movie.java
@@ -3,7 +3,13 @@ public class Movie {
     private String actor;
     private int year;
     private String directors;
-    
+
+    private static String RESET = "\u001B[0m";
+    private static String RED = "\u001B[31m";
+    private static String GREEN = "\u001B[32m";
+    private static String BLUE = "\u001B[34m";
+    private static String YELLOW = "\u001B[33m";
+    private static String PURPLE = "\u001B[35m";
 
     public static void main(String[] args) {
         
@@ -33,6 +39,6 @@ public class Movie {
     }
 
     public String toString(){
-        return (getTitle() + " " + getActor() + " " + getYear() + " " + getDirector());
+        return (BLUE + getTitle() + RESET + " " + getActor() + " " + YELLOW + getYear() + " " + RESET + getDirector());
     }
 }
